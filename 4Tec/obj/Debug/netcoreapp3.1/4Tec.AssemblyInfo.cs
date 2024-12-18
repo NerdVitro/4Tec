b27656e4d9bebfcc9fa2f5c7defee34f0302a7b1
Vitor Souza <104155312+NerdVitro@users.noreply.github.com>
2024-12-18 16:51:54 -0300

    test

--- 4Tec/obj/Debug/netcoreapp3.1/4Tec.AssemblyInfo.cs
+++ 4Tec/obj/Debug/netcoreapp3.1/4Tec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4Tec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67dcf4e2c0898e4c2104452604a542343875e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d0accae3590a4e783ad94907fcfedd44c76cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("4Tec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4Tec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
